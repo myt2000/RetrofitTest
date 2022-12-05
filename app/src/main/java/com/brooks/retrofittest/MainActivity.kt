7e907ebc6095e646553adf4443a3020025965215
brooks <myt2000@126.com>
2022-12-05 16:01:01 +0800

    结果显示在页面的效果图

--- app/src/main/java/com/brooks/retrofittest/MainActivity.kt
+++ app/src/main/java/com/brooks/retrofittest/MainActivity.kt
@@ -9,33 +9,47 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
+    var string: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getAppDataBtn.setOnClickListener {
             val retrofit = Retrofit.Builder()
-                .baseUrl("http://192.168.16.30")
+                .baseUrl("http://192.168.16.30/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
             val appService = retrofit.create(AppService::class.java)
             appService.getAppData().enqueue(object : Callback<List<App>> {
                 override fun onResponse(call: Call<List<App>>, response: Response<List<App>>) {
+
                     val list = response.body()
+
                     if (list!=null) {
                         for (app in list) {
+                            var stringData = "" + app.id + " " + app.name + " " + app.version + "\n "
+                            string += stringData
                             Log.d("Gson数据解析", "id is ${app.id} ")
                             Log.d("Gson数据解析", "name is ${app.name} ")
                             Log.d("Gson数据解析", "version is ${app.version} ")
                         }
                     }
+                    string?.let { showRespones(it) }
                 }
 
                 override fun onFailure(call: Call<List<App>>, t: Throwable) {
                     t.printStackTrace()
                 }
             })
+        }
+    }
+
+    private fun showRespones(response: String) {
+        runOnUiThread {
+            // 在这里进行UI操作，将结果显示到界面上
+            responseText.text = response
         }
     }
 }
